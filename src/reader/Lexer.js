bc486987fca5eddfd429c8c4bbd4b69f022b7766
Jason Barr <jason.barr@metronet.com>
2023-01-13 17:13:20 -0600

    Construct Lexer class

---
+++ src/reader/Lexer.js
@@ -0,0 +1,13 @@
+/**
+ * Class holding the functions that tokenize an input string
+ * @property {String} input
+ */
+exports.Lexer = class Lexer {
+  /**
+   * Constructs the Lexer object
+   * @param {String} input program to lex
+   */
+  constructor(input) {
+    this.input = input;
+  }
+};
