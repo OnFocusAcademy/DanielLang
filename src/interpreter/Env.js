3fce4772ad56c5eaf2f72de713e7896c69ccfcee
Jason Barr <jason.barr@metronet.com>
2023-01-16 12:09:04 -0600

    Define class for interpreter environment

---
+++ src/interpreter/Env.js
@@ -0,0 +1,96 @@
+/**
+ * Environment to pass into interpreter
+ */
+exports.Env = class Env {
+  /**
+   * Constructs an Environment object
+   * @param {Object} params
+   * @param {Env|null} params.parent
+   * @param {String} params.name
+   */
+  constructor({ parent = null, name = "global" } = {}) {
+    this.parent = parent;
+    this.name = name;
+    this.namespace = new Map();
+  }
+
+  /**
+   * Creates an environment and adds each name/value pair in vars to its namespace
+   * @param {Object} vars
+   * @param {Object} params
+   * @param {Env|null} params.parent
+   * @param {String} params.name
+   * @returns {Env}
+   */
+  static from(vars, { parent = null, name = "global" } = {}) {
+    let env = new Env({ parent, name });
+
+    for (let sym of Object.getOwnPropertySymbols(vars)) {
+      env.set(sym, vars[sym]);
+    }
+
+    for (let [key, value] of Object.entries(vars)) {
+      env.set(Symbol.for(key), value);
+    }
+
+    return env;
+  }
+
+  /**
+   * Extend the current Environment by defining a child
+   * @param {Env} parent
+   * @returns {Env}
+   */
+  extend(parent, name) {
+    return new Env({ parent, name });
+  }
+
+  /**
+   * Gets the value for name in the current env's namespace
+   * @param {Symbol} key
+   * @returns {import("../reader/read").AST|Object}
+   */
+  get(key) {
+    const env = this.lookup(key);
+    return env.namespace.get(key);
+  }
+
+  /**
+   * Checks if the current env's namespace contains key
+   * @param {Symbol} key
+   * @returns {Boolean}
+   */
+  has(key) {
+    return this.namespace.has(key);
+  }
+
+  /**
+   * Returns the environment whose namespace contains key
+   * @param {Symbol} key
+   * @returns {Env}
+   */
+  lookup(key) {
+    let current = this;
+
+    while (current) {
+      if (current.has(key)) {
+        return current;
+      }
+    }
+
+    throw new Error(
+      `Name ${key.description} not found in the current environment`
+    );
+  }
+
+  /**
+   * Sets a name to a value in the current env's namespace
+   * @param {Symbol} key
+   * @param {import("../reader/read").AST|Object} value
+   * @returns {Boolean}
+   */
+  set(key, value) {
+    this.namespace.set(key, value);
+    return this.namespace.has(key);
+  }
+};
