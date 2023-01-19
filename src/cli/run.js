396104d3c723e3ad900592af051d924e6777ffa8
Jason Barr <jason.barr@metronet.com>
2023-01-19 07:58:29 -0600

    Allow evaluating a file as code

--- src/cli/run.js
+++ src/cli/run.js
@@ -1,3 +1,24 @@
+const path = require("path");
+const { read } = require("../reader/read");
+const { evaluate } = require("../interpreter/evaluate");
 const { repl } = require("./repl");
+const { global } = require("./global");
+const { readfile } = require("./utils");
 
-exports.run = () => repl();
+exports.run = (argv) => {
+  switch (typeof argv[0]) {
+    case "string": {
+      const [pathString] = argv[0];
+      const code = readfile(
+        pathString.startsWith("//")
+          ? path.join(process.cwd(), pathString)
+          : pathString
+      );
+      let env = global.extend("<main>");
+      return evaluate(read(code), env);
+    }
+
+    default:
+      repl();
+  }
+};
