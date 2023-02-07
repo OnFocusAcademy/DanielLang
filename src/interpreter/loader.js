ac3402a56e86fa0e413e665cb3b118e666b2f8c6
Jason Barr <jason.barr@metronet.com>
2023-02-07 07:33:08 -0600

    Sort module dependencies to load in correct order

---
+++ src/interpreter/loader.js
@@ -0,0 +1,91 @@
+const { resolveRequire, Exception } = require("../runtime");
+
+let moduleTable = {};
+let nameMap = {};
+let modules = {};
+
+/**
+ * Resolve module names to their file paths
+ * @param {String[]} requires
+ * @param {String[]} nativeRequires
+ */
+const getModulePaths = (requires, nativeRequires) => {
+  let urls = [];
+
+  for (let req of requires) {
+    let url = resolveRequire(req);
+    nameMap[url] = req;
+    urls.push(url);
+  }
+
+  for (let req of nativeRequires) {
+    let url = resolveRequire(req, { native: true });
+    nameMap[url] = req;
+    urls.push(url);
+  }
+
+  return urls;
+};
+
+/**
+ * Traverse the dependency graph and get the order in which to load modules
+ * Inspiration from {@link https://github.com/brownplt/pyret-lang/blob/331e29e79cfc8d49fd3b73684dd3a034d445f5c5/src/js/base/amd_loader.js#L73}
+ * @param {String[]} deps
+ * @returns {String[]}
+ */
+const getLoadOrder = (deps) => {
+  let sorted = [];
+  let toVisit = {};
+  let currentlyVisiting = {};
+  let visited = {};
+
+  const visitNode = (node) => {
+    if (!(node in moduleTable) && !(node in modules)) {
+      throw new Exception(`Unknown module ${nameMap[node]}`);
+    }
+
+    if (node in currentlyVisiting) {
+      throw new Exception(
+        `You have a circular dependency that includes ${nameMap[node]}`
+      );
+    }
+
+    if (node in visited) {
+      // it's alreaady been loaded, so we're good
+      return;
+    }
+
+    // if we get here, the node is unvisited so we mark it as currently being visited
+    currentlyVisiting[node] = true;
+
+    if (node in toVisit) {
+      // we're visiting it now, so we can remove it from toVisit
+      delete toVisit[node];
+    }
+
+    // now visit its children recursively
+    for (let dep of moduleTable[node].deps) {
+      visitNode(dep);
+    }
+
+    // now all of its dependencies will have been added to the list
+    // so it's safe to add it to the load order array
+    sorted.push(node);
+    delete currentlyVisiting[node];
+    visited[node] = true;
+  };
+
+  // now flag the nodes children as toVisit
+  for (let dep of deps) {
+    toVisit[dep] = true;
+  }
+
+  // as long as there are keys in toVisit, we need to keep visiting
+  while (Object.keys(toVisit).length > 0) {
+    let nextNode = Object.keys()(toVisit)[0];
+    visitNode(nextNode);
+  }
+
+  // now the dependencies have been sorted, so we can return the list
+  return sorted;
+};
