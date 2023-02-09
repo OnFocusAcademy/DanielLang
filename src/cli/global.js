417e083812aec586bc89afcc04f91470a45c1b77
Jason Barr <jason.barr@metronet.com>
2023-02-09 15:23:33 -0600

    Add module in Daniel and load into Global environment

--- src/cli/global.js
+++ src/cli/global.js
@@ -5,7 +5,9 @@ const { evaluate } = require("../interpreter/evaluate");
 
 const createGlobalEnv = () => {
   const env = new Env({ parent: null, name: "__global__" });
-  loadModules({ name: "Global", env, native: true, open: true });
+  loadModules({ name: "Core", env, native: true, open: true, evaluate });
+  loadModules({ name: "String", env, native: true, evaluate });
+  loadModules({ name: "Base", env, open: true, evaluate });
   env.set(
     Symbol.for("eval"),
     makeFunction((ast) => evaluate(ast, env), { name: "Core.eval" })
